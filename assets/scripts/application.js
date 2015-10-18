28cd94c46f12bd36b2d0f166a0fda21ab6b5b332
Edward Loveall <edward@edwardloveall.com>
2015-10-18 13:17:49 -0400

    Detect unsupported browsers
    If the browser is lower than IE 11 or Android 4.4, use the body class 'unsupported'. I'll style all this later, but for now, just detecting.

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -13,11 +13,30 @@ var Detector = (function () {
     key: 'setBodyClass',
     value: function setBodyClass() {
       var body = document.getElementsByTagName('body')[0];
+
+      if (this.isUnsupported(bowser.name, bowser.version)) {
+        body.className = 'unsupported';
+        return;
+      }
+
       var browserName = bowser.name.toLowerCase();
+
       if (body.className === '') {
         body.className = browserName;
       } else {
         body.className += ' ' + browserName;
+      }
+    }
+  }, {
+    key: 'isUnsupported',
+    value: function isUnsupported(name, version) {
+      var floatVersion = parseFloat(version);
+      if (name === "Internet Explorer" && floatVersion < 11) {
+        return true;
+      } else if (name === "Android" && floatVersion < 4.4) {
+        return true;
+      } else {
+        return false;
       }
     }
   }]);
--- assets/scripts/es2015/detector.js
+++ assets/scripts/es2015/detector.js
@@ -1,11 +1,29 @@
 class Detector {
   static setBodyClass() {
     let body = document.getElementsByTagName('body')[0];
+
+    if (this.isUnsupported(bowser.name, bowser.version)) {
+      body.className = 'unsupported';
+      return;
+    }
+
     let browserName = bowser.name.toLowerCase();
+
     if (body.className === '') {
       body.className = browserName;
     } else {
       body.className += ' ' + browserName;
+    }
+  }
+
+  static isUnsupported(name, version) {
+    let floatVersion = parseFloat(version);
+    if (name === "Internet Explorer" && floatVersion < 11) {
+      return true;
+    } else if (name === "Android" && floatVersion < 4.4) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
